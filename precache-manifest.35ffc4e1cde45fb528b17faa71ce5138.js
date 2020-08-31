5c2b455eef60c975430864e8055d8bb9cb074f12
Yesika Gomez <yesika.go@hotmail.com>
2020-08-30 22:05:51 -0500

    Updates

---
+++ precache-manifest.35ffc4e1cde45fb528b17faa71ce5138.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc1c898226da36630cb5621324d5ba8",
+    "url": "/yesikagomez/citas-aleatorias/index.html"
+  },
+  {
+    "revision": "68e0629d5a4de7acd76d",
+    "url": "/yesikagomez/citas-aleatorias/static/css/2.240f97b7.chunk.css"
+  },
+  {
+    "revision": "5ccd05f4ae12cfbe1339",
+    "url": "/yesikagomez/citas-aleatorias/static/css/main.0e05f2cb.chunk.css"
+  },
+  {
+    "revision": "68e0629d5a4de7acd76d",
+    "url": "/yesikagomez/citas-aleatorias/static/js/2.c7c0d617.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yesikagomez/citas-aleatorias/static/js/2.c7c0d617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ccd05f4ae12cfbe1339",
+    "url": "/yesikagomez/citas-aleatorias/static/js/main.7ce1910d.chunk.js"
+  },
+  {
+    "revision": "637cfdf0c76b51f979f8",
+    "url": "/yesikagomez/citas-aleatorias/static/js/runtime-main.fdfa091a.js"
+  }
+]);
